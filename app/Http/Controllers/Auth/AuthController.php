b4ab95d7fd9500e60d482cbaa01d88e8cdf9195f
PilotJinix <agungsantoso557@gmail.com>
2021-02-03 08:54:02 +0700

    Data

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -35,6 +36,40 @@ class AuthController extends Controller
         ]);
 
         $this->response['message'] = 'success';
+        return response()->json($this->response, 200);
+    }
+
+    public function login(Request $req)
+    {
+        $req->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $req->username)->first();
+
+        if (!$user || ! Hash::check($req->password, $user->password)) {
+            return response()->json([
+                'message' => "failed"
+            ]);
+        }
+
+        $token =  $user->createToken()->plainTextToken;
+        $this->response['message'] = 'success';
+        $this->response['data'] = [
+            'token' => $token
+        ];
+
+        return response()->json($this->response, 200);
+    }
+
+    public function check()
+    {
+        $user = Auth::user();
+
+        $this->response['message'] = 'success';
+        $this->response['data'] = $user;
+
         return response()->json($this->response, 200);
     }
 
